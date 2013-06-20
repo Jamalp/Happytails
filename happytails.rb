9bc77e0ceec8dcab69f1a6482e44a336274038c6
Jamal Powell <jamalkpowell@gmail.com>
2013-06-20 19:29:11 -0400

    update

--- happytails.rb
+++ happytails.rb
@@ -10,23 +10,23 @@ class Client
   end
 
   def to_s
-        "#{@name} is an #{@age} year old #{@gender} with #{@kids} kids and #{@pets} pets"
+    "#{@name} is an #{@age} year old #{@gender} with #{@kids} kids and #{@pets} pets"
   end
+
 end
 
 jamal = Client.new("Jamal", 26, "male", 1)
 puts jamal
 
+def giving(name, age, breed, gender, fav_toys)
+  puts "An #{Animal} is being given to the shelter"
+  return Animal.new
+end
 
-  def giving(name, age, breed, gender, fav_toys)
-    puts "An #{Animal} is being given to the shelter"
-    return Animal.new
-  end
-
-  def adoption(name, age, breed, gender, fav_toys)
-    puts "A #{Animal} is being adopted!"
-    #Remove an animal from "available pets array"
-  end
+def adoption(name, age, breed, gender, fav_toys)
+  puts "A #{Animal} is being adopted!"
+  #Remove an animal from "available pets array"
+end
 
 
 class Animal
@@ -40,27 +40,35 @@ class Animal
     @fav_toys = fav_toys
   end
 
-  # puts "#{name} is a #{age} year old #{gender} #{breed} & his favorite toy is #{fav_toys}."
-
   def to_s
-    "#{name} is a #{age} year old #{gender} #{breed} who loves to play with his #{fav_toys}!"
+    "#{name} is a #{age} year old #{gender} #{breed} who loves to play with his/her #{fav_toys}!"
     #return animal
   end
 
-  def list_clients
-    clients.list do |client|
-      puts client
-  end
+  boogie = Animal.new("Boogie", "Golden Retriever", "1", "Female", "Doll")
+  puts boogie
 
-  def list_animals
-    animals.list do |animal|
-      puts animal
-  end
-
+  sparky = Animal.new("sparky", "doberman", 7, "male", "frisbee")
+  puts sparky
 end
 
-sparky = Animal.new("sparky", "doberman", 7, "male", "frisbee")
-puts sparky
+def list_clients
+  clients.list do |client|
+    puts client
+  end
+end
+
+def list_pets(adoptable_pets)
+  puts "Adoptable pets:\n"
+  adoptable_pets.each { |pet| puts pet }
+end
+
+def list_animals(available_pets)
+  animals.list do |animal|
+    puts animal
+  end
+end
+
 # class Shelter
 #   attr_accessor :available_pets, :unavailable_pets
 
@@ -77,23 +85,25 @@ puts sparky
 #     available_pets = []
 #   end
 
-# end
+#end
 
-# #Client List
+Client List
 
 clients << Client.new("Mike Lemon", 37, "Male", 4)
 clients << Client.new("Sally Fields", "20", "female", "0")
 clients << Client.new("Tom Tom", "78", "Male", "2")
 clients << Client.new("George Micheal Blueth", "19", "Male", "0")
 
-# available_pets << Animal.new("snoopy", "Beagle", 4, "Male", "[Bone, frisbee")
-# available_pets << Animal.new("snoopy", "Beagle", 4, "Male", "ball")
-# available_pets << Animal.new("snoopy", "Beagle", 4, "Male", "[rope, ipad]")
+available_pets << Animal.new("snoopy", "Beagle", 4, "Male", "[Bone, frisbee")
+available_pets << Animal.new("snoopy", "Beagle", 4, "Male", "ball")
+available_pets << Animal.new("snoopy", "Beagle", 4, "Male", "[rope, ipad]")
 
-# unavailable_pets << Animal.new("snoopy", "Beagle", 4, "Male", "[rope, ipad]")
-# unavailable_pets << Animal.new("snoopy", "Beagle", 4, "Male", "[rope, ipad]")
-# unavailable_pets << Animal.new("snoopy", "Beagle", 4, "Male", "[rope, ipad]")
+unavailable_pets << Animal.new("snoopy", "Beagle", 4, "Male", "[rope, ipad]")
+unavailable_pets << Animal.new("snoopy", "Beagle", 4, "Male", "[rope, ipad]")
+unavailable_pets << Animal.new("snoopy", "Beagle", 4, "Male", "[rope, ipad]")
 
 
+luigi = Client.new("Luigi", "8", "Male", 0)
+puts "A client walks in.."
+puts luigi
 
-# # puts client_1.pets[:snoopy].available_pets
